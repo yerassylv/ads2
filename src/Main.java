ad86277acece58d7bd51eb11e7237a95af04a4d8
yerassylv <bolatkannn@gmail.com>
2024-04-27 02:45:23 +0600

    Completion of the test  of MyArrayList

--- src/Main.java
+++ src/Main.java
@@ -22,6 +22,27 @@ public class Main {
         aL.removeLast();
         System.out.println("Remove last element of array list: ");
         aL.printArr();
+        aL.removeFirst();
+        System.out.println("Remove first element of array list: ");
+        aL.printArr();
+        aL.addFirst(2);
+        aL.addLast(7);
+        System.out.println("Add first and last element of array list: ");
+        aL.printArr();
+        System.out.println(aL.indexOf(7));
+        aL.add(1);
+        aL.add(9);
+        aL.add(2);
+        aL.set(3,11);
+        System.out.println("Set element at index 3: ");
+        aL.printArr();
+        aL.sort();
+        System.out.println("Sort array list: ");
+        aL.printArr();
+        System.out.println("Exist of element 11: " + aL.exist(11));
+
+
+
 
 
 
